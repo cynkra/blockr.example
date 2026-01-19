6a06c4a5e3bf6edb7aaa578d6f7d8fd7cd04b9d2
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2026-01-19 16:28:18 +0100

    Populate

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,13 @@
 register_example_blocks <- function() {
-  register_blocks()
+  register_blocks(
+    "new_head_block",
+    name = "head block",
+    description = "Returns first n rows in data",
+    category = "transform",
+    icon = "eye",
+    package = utils::packageName(),
+    overwrite = TRUE
+  )
 }
 
 .onLoad <- function(libname, pkgname) {
