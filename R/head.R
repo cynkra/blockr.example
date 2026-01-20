8ef6800024042ecaf111f1418d35027eb9a8faa0
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2026-01-20 16:28:47 +0100

    Format

--- R/head.R
+++ R/head.R
@@ -9,11 +9,10 @@
 new_example_head_block <- function(n = 6L, ...) {
   new_block(
     ui = function(id) {
-      # Always return a shiny tagList object
       tagList(
         numericInput(
           inputId = NS(id, "n"),
-          label = "Select no. of rows",
+          label = "Select no. rows",
           value = n,
           min = 1L
         ),
@@ -22,23 +21,18 @@ new_example_head_block <- function(n = 6L, ...) {
     },
     server = function(id, data) {
       moduleServer(id, function(input, output, session) {
-        # Track inputs
         n_rows = reactiveVal(n)
         observeEvent(input$n, n_rows(input$n))
 
-        # Render outputs
-        output$result <- DT::renderDT({
+        output$result <- DT::renderDT(
           expr = utils::head(data(), n = n_rows())
-        })
+        )
 
-        # Return list w/ expression and state
         list(
-          expr = reactive(bquote(
-            utils::head(x = .(data), n = .(n), list(n = n_rows))
-          )),
-          state = list(
-            n = n_rows
-          )
+          expr = reactive(
+            bquote(utils::head(x = .(data), n = .(n), list(n = n_rows)))
+          ),
+          state = list(n = n_rows)
         )
       })
     },
